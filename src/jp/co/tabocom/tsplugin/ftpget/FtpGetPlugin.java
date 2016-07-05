b2fc2b5cb2b8e039157a22a53f1fcdc5b1b1559a
turbou <turbou@i.softbank.jp>
2016-07-05 14:39:01 +0900

    project settings update.

--- src/jp/co/tabocom/tsplugin/ftpget/FtpGetPlugin.java
+++ src/jp/co/tabocom/tsplugin/ftpget/FtpGetPlugin.java
@@ -5,24 +5,13 @@ import java.util.List;
 import jp.co.tabocom.teratermstation.model.TargetNode;
 import jp.co.tabocom.teratermstation.plugin.TeratermStationPlugin;
 import jp.co.tabocom.teratermstation.ui.action.TeratermStationAction;
-import jp.co.tabocom.teratermstation.ui.action.TeratermStationBulkAction;
+import jp.co.tabocom.teratermstation.ui.action.TeratermStationContextMenu;
 
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.preference.PreferenceStore;
-import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.swt.widgets.Shell;
 
 public class FtpGetPlugin implements TeratermStationPlugin {
-
-    @Override
-    public List<TeratermStationAction> getActions(TargetNode node, Shell shell, ISelectionProvider selectionProvider) {
-        return null;
-    }
-
-    @Override
-    public List<TeratermStationBulkAction> getBulkActions(List<TargetNode> nodeList, Shell shell) {
-        return null;
-    }
 
     @Override
     public PreferencePage getPreferencePage() {
@@ -35,5 +24,23 @@ public class FtpGetPlugin implements TeratermStationPlugin {
 
     @Override
     public void teminate(PreferenceStore preferenceStore) throws Exception {
+    }
+
+    @Override
+    public List<TeratermStationContextMenu> getActions(TargetNode[] nodes, Shell shell) {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+    @Override
+    public List<TeratermStationAction> getBulkActions(TargetNode[] nodes, Shell shell) {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+    @Override
+    public List<TeratermStationContextMenu> getDnDActions(TargetNode[] nodes, Object value, Shell shell) {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
     }
 }
